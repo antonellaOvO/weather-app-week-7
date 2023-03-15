3c6597e7ce54cd8ee242643b0615db9c5d6b7cc2
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2023-03-15 20:21:33 +0100

    Added icon from API
    The principal icon changes from the API's information.

--- src/weather-app.js
+++ src/weather-app.js
@@ -1,5 +1,5 @@
 let key = "3f6be1c407b0d9d1933561808db358ba";
-let city = "barcelona";
+let city = "Barcelona";
 let units = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
 
@@ -61,7 +61,30 @@ function displayTempDate(response) {
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = Math.round(response.data.main.humidity);
+
+  let iconTempNow = document.querySelector("#image-temp-now");
+  iconTempNow.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconTempNow.setAttribute("alt", response.data.weather[0].description);
+
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
 }
 axios.get(apiUrl).then(displayTempDate);
+
+function search(city) {
+  let key = "3f6be1c407b0d9d1933561808db358ba";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
+  axios.get(apiUrl).then(displayTempDate);
+}
+function submit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  search(city.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submit);
